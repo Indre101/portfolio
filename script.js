598fb186d483b9b99a017ab9270d79b596d9cd95
indre101 <zygaityte.indre@gmail.com>
2020-01-13 16:47:38 +0100

    added functions for clicked classs

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
 const menuButton = document.getElementById("menuButton");
+const openMenuWindow = document.getElementById("openMenuWindow");
+const menuIconDashes = document.querySelectorAll(".dash");
+
+let i = 0;
+
+menuButton.onclick = function () {
+  i++
+  menuIconDashes.forEach(dash => {
+    setTimeout(() => {
+      toggleClassClicked(dash);
+    }, 150);
+  });
+
+
+  animateMenuToslideIn(openMenuWindow);
+  removeUNClickedClass(menuButton);
+  addClickedClass(menuButton);
+
+
+  if (i == 2) {
+    addUNClickedClass(menuButton)
+    removeClickedClass(menuButton)
+    animateMenuToslideOut(openMenuWindow)
+    i = 0;
+  }
+}
+
+function animateMenuToslideIn(elementToAnimate) {
+  elementToAnimate.classList.add("d-flex");
+  elementToAnimate.classList.add("slideUpAnimation");
+  elementToAnimate.classList.remove("d-none");
+  elementToAnimate.classList.remove("slideDownAnimation");
+}
+
+
+function animateMenuToslideOut(elementToAnimate) {
+  elementToAnimate.classList.remove("slideUpAnimation");
+  elementToAnimate.classList.add("slideDownAnimation");
+  setTimeout(() => {
+    elementToAnimate.classList.add("d-none");
+    elementToAnimate.classList.remove("d-flex");
+  }, 1000);
+}
+
+
+function addClickedClass(elementToAddTheClass) {
+  elementToAddTheClass.classList.add("clicked");
+}
+
+
+function removeClickedClass(elementToAddTheClass) {
+  elementToAddTheClass.classList.remove("clicked");
+}
+
+
+function addUNClickedClass(elementToAddTheClass) {
+  elementToAddTheClass.classList.add("unclicked");
+}
+
+
+function removeUNClickedClass(elementToAddTheClass) {
+  elementToAddTheClass.classList.remove("unclicked");
+}
+
+
+function toggleClassClicked(elementToAddTheClass) {
+  elementToAddTheClass.classList.toggle("clicked");
+}
