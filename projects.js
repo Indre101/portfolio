1cc01efb0604c8bb1ba89e9b708639a3e65b7941
indre101 <zygaityte.indre@gmail.com>
2020-01-26 20:36:47 +0100

    ditched the double animation

--- projects.js
+++ projects.js
@@ -3,35 +3,44 @@
 const textCaption = document.querySelector(".textCaption")
 const imageIntheCarousel = document.querySelector(".imageIntheCarousel")
 
+addAnimationToTheActiveSlide(textCaption, imageIntheCarousel)
 
-changeImageAndTextInTheImageSlider(textCaption, imageIntheCarousel)
-
-function changeImageAndTextInTheImageSlider(text, image) {
-
+function addAnimationToTheActiveSlide(text, image) {
   text.classList.add("changeWidth");
   image.classList.add("changeWidth");
-  text.addEventListener("webkitAnimationEnd", changeImageCaption)
-  image.addEventListener("webkitAnimationEnd", changeTheImage)
-
-}
-
-function changeImageCaption(e) {
-
-  e.target.style.animationIterationCount = 2;
-  e.target.textContent = "Image name";
-  e.target.classList.add("textCaptionSecond");
-}
-
-function changeTheImage(e) {
-
-  e.target.style.animationIterationCount = 2;
-  e.target.style.backgroundImage = "url('./images/photos/project-images/chad.png')"
-
 }
 
 
-const RandomColorBlueOrOrange = () => {
 
-  // const color = 240
+// const RandomColorBlueOrOrange = (colorPlaceInColorWheel) => {
 
-}
+//   const color = `rgba(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*155)}, ${colorPlaceInColorWheel})`;
+//   return color
+
+// }
+
+// changeImageAndTextInTheImageSlider(textCaption, imageIntheCarousel)
+
+// function changeImageAndTextInTheImageSlider(text, image) {
+
+//   text.classList.add("changeWidth");
+//   image.classList.add("changeWidth");
+//   // text.style.backgroundColor = RandomColorBlueOrOrange(210);
+//   text.addEventListener("webkitAnimationEnd", changeImageCaption)
+//   image.addEventListener("webkitAnimationEnd", changeTheImage)
+
+// }
+
+// function changeImageCaption(e) {
+
+//   // e.target.style.animationIterationCount = 2;
+//   e.target.textContent = "Image name";
+//   e.target.classList.add("textCaptionSecond");
+// }
+
+// function changeTheImage(e) {
+
+//   // e.target.style.animationIterationCount = 2;
+//   e.target.style.backgroundImage = "url('./images/photos/project-images/chad.png')"
+
+// }
