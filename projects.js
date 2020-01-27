c7c904e9db69313f1a5e2ffa5d1a84f099e3925b
indre101 <zygaityte.indre@gmail.com>
2020-01-27 14:01:10 +0100

    changed the carousel layout

--- projects.js
+++ projects.js
@@ -2,6 +2,8 @@
 
 const textCaption = document.querySelector(".textCaption")
 const imageIntheCarousel = document.querySelector(".imageIntheCarousel")
+const imageAndTextCaption = document.querySelectorAll(".imageAndTextCaption")
+const projectImagesAndCaption = document.querySelector(".projectImages")
 
 addAnimationToTheActiveSlide(textCaption, imageIntheCarousel)
 
@@ -9,38 +11,3 @@ function addAnimationToTheActiveSlide(text, image) {
   text.classList.add("changeWidth");
   image.classList.add("changeWidth");
 }
-
-
-
-// const RandomColorBlueOrOrange = (colorPlaceInColorWheel) => {
-
-//   const color = `rgba(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*155)}, ${colorPlaceInColorWheel})`;
-//   return color
-
-// }
-
-// changeImageAndTextInTheImageSlider(textCaption, imageIntheCarousel)
-
-// function changeImageAndTextInTheImageSlider(text, image) {
-
-//   text.classList.add("changeWidth");
-//   image.classList.add("changeWidth");
-//   // text.style.backgroundColor = RandomColorBlueOrOrange(210);
-//   text.addEventListener("webkitAnimationEnd", changeImageCaption)
-//   image.addEventListener("webkitAnimationEnd", changeTheImage)
-
-// }
-
-// function changeImageCaption(e) {
-
-//   // e.target.style.animationIterationCount = 2;
-//   e.target.textContent = "Image name";
-//   e.target.classList.add("textCaptionSecond");
-// }
-
-// function changeTheImage(e) {
-
-//   // e.target.style.animationIterationCount = 2;
-//   e.target.style.backgroundImage = "url('./images/photos/project-images/chad.png')"
-
-// }
