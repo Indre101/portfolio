490c937adc0ef057256168f4721dccf71b20b6c4
indre101 <zygaityte.indre@gmail.com>
2020-01-28 18:21:15 +0100

    added animation to the main container

--- projects.js
+++ projects.js
@@ -5,5 +5,3 @@ const imageIntheCarousel = document.querySelector(".imageIntheCarousel");
 const imageAndTextCaption = document.querySelectorAll(".imageAndTextCaption");
 const imageAndTextCaptionConvertedArray = Array.from(imageAndTextCaption);
 const projectImagesAndCaptions = document.querySelector(".projectImages");
-
-let numb = 1;
