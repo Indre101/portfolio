ce7af2d8bf71f352ca93877e80a7327335cbbb0f
indre101 <zygaityte.indre@gmail.com>
2020-01-28 15:50:43 +0100

    adds animation frame

--- projects.js
+++ projects.js
@@ -6,27 +6,58 @@ const imageAndTextCaption = document.querySelectorAll(".imageAndTextCaption");
 const imageAndTextCaptionConvertedArray = Array.from(imageAndTextCaption);
 const projectImagesAndCaptions = document.querySelector(".projectImages");
 
-const evenChildren = document.querySelectorAll(
-  ".imageAndTextCaption:nth-child(even)"
-);
-const oddChildren = document.querySelectorAll(
-  ".imageAndTextCaption:nth-child(odd)"
-);
+let numb = 1;
 
-window.onload = function() {
-  addClasess();
-};
+// function animate({ duration, timing, draw }) {
+//   let start = performance.now();
 
-function addClasess() {
-  evenChildren.forEach(child => {
+//   console.log(start);
+//   requestAnimationFrame(animate());
+//   // timeFraction goes from 0 to 1
+//   let timeFraction = (time - start) / duration;
+//   if (timeFraction > 10000) timeFraction = 1000;
+
+//   // calculate the current animation state
+//   let progress = timing(timeFraction);
+
+//   draw(progress); // draw it
+
+//   if (timeFraction < 1) {
+//     requestAnimationFrame(animate);
+//   }
+// }
+
+// imageAndTextCaption.forEach(e => {
+//   animate({
+//     duration: 1000,
+//     timing(timeFraction) {
+//       return timeFraction;
+//     },
+//     draw(progress) {
+//       e.style.transform = `translateX(-${progress}%) rotateY(50deg) skewY(11deg) translateZ(-4vw)`;
+//     }
+//   });
+// });
+
+let myReq;
+
+function repeatOften() {
+  numb++;
+  imageAndTextCaption.forEach(e => {
+    console.log(numb);
     setTimeout(() => {
-      child.classList.remove("right-side-Animation");
-      child.classList.add("left-Slide-animation");
-
-      setTimeout(() => {
-        child.classList.add("right-side-Animation");
-        child.classList.remove("left-Slide-animation");
-      }, 3000);
+      e.style.transform = `translateX(-${numb}%)`;
     }, 1000);
   });
+  // console.log("object");
+  myReq = requestAnimationFrame(repeatOften);
+}
+// requestAnimationFrame(repeatOften);
+myReq = requestAnimationFrame(repeatOften);
+
+document.querySelector("button").addEventListener("click", cancelAnimatio);
+
+function cancelAnimatio() {
+  console.log("kæløæø");
+  cancelAnimationFrame(myReq);
 }
