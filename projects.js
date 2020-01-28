72b651e02edaf4ed905e000ab722660361b132fd
indre101 <zygaityte.indre@gmail.com>
2020-01-28 11:31:38 +0100

    geeting values of tranforms

--- projects.js
+++ projects.js
@@ -6,58 +6,36 @@ const imageAndTextCaption = document.querySelectorAll(".imageAndTextCaption");
 const imageAndTextCaptionConvertedArray = Array.from(imageAndTextCaption);
 const projectImagesAndCaptions = document.querySelector(".projectImages");
 
-let htmlStyles = window.getComputedStyle(document.querySelector("html"));
-let colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
+// let htmlStyles = window.getComputedStyle(document.querySelector("html"));
+// let colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
 
-window.onload = function() {
-  document.documentElement.style.setProperty(
-    "--colNum",
-    imageAndTextCaption.length
-  );
-};
-
-// const previousArrow = document.querySelector(".previousArrow");
-// const nextArrow = document.querySelector(".nextArrow");
-
-// const activeProjectSlide = () => {
-//   let activeSlide;
-//   imageAndTextCaption.forEach(element => {
-//     if (element.classList.contains("active")) {
-//       activeElement = element;
-//     }
-//   });
-//   return activeElement;
+// window.onload = function() {
+//   document.documentElement.style.setProperty(
+//     "--colNum",
+//     imageAndTextCaption.length
+//   );
 // };
 
-// let activeSlide = activeProjectSlide();
-// let clickCount = imageAndTextCaptionConvertedArray.indexOf(activeSlide);
+imageAndTextCaption.forEach(slide => {
+  slide.addEventListener("mouseover", stopAnimation);
+});
 
-// nextArrow.onclick = function() {
-//   clickCount++;
-//   console.log(clickCount);
-//   imageAndTextCaption[clickCount].classList.add("active");
-//   if (!findPreviousSibling(clickCount) || !findNextSibling(clickCount)) {
-//     return false;
-//   } else if (findPreviousSibling(clickCount)) {
-//     console.log("true");
-//   } else if (findNextSibling(clickCount)) {
-//     console.log("true");
-//   }
+function stopAnimation() {
+  imageAndTextCaption.forEach(slide => {});
+}
 
-//   // let previousSibling = findPreviousSibling(clickCount);
-//   // let nextSibling = findNextSibling(clickCount);
-// };
+imageAndTextCaption.forEach(slide => {
+  slide.addEventListener("mouseout", continueAnimation);
+});
 
-// function findPreviousSibling(activeSlideIndex) {
-//   let previousSiblings =
-//     imageAndTextCaptionConvertedArray[activeSlideIndex].previousElementSibling;
-//   previousSiblings.classList.add("right-Slide");
-//   return previousSiblings;
-// }
+function continueAnimation() {
+  imageAndTextCaption.forEach(slide => {});
+}
 
-// function findNextSibling(activeSlideIndex) {
-//   let previousSiblings =
-//     imageAndTextCaptionConvertedArray[activeSlideIndex].nextElementSibling;
-//   previousSiblings.classList.add("left-Slide");
-//   return previousSiblings;
-// }
+function getTheStylePropertiesOfTransforms(slide) {
+  var computedStyle = window.getComputedStyle(slide);
+  let translatedX = computedStyle.getPropertyValue("translateX");
+  let skewedY = computedStyle.getPropertyValue("skewY");
+  let rotated = computedStyle.getPropertyValue("rotateY");
+  console.log(translatedX, skewedY, rotated);
+}
