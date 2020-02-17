044fa6d4c1bded33018994bf9fe8abfb2296382a
indre101 <zygaityte.indre@gmail.com>
2020-02-17 15:09:05 +0100

    test

--- projects.js
+++ projects.js
@@ -1,12 +1,14 @@
 // PROJECTS PAGE
-"use strict"
-const HTML = {}
+"use strict";
+const HTML = {};
 
 function getHTMLelements() {
   HTML.projectImagesAndCaptions = document.querySelector(".projectImages");
   HTML.imageModalContainer = document.querySelector(".imageModalContainer");
   HTML.imageModal = document.querySelector(".imageModal");
-  HTML.studentCardTemplate = document.querySelector(".studentCardTemplate").content
+  HTML.studentCardTemplate = document.querySelector(
+    ".studentCardTemplate"
+  ).content;
 }
 
 window.addEventListener("DOMContentLoaded", init);
@@ -15,20 +17,19 @@ function init() {
   fetch("projects.json")
     .then(res => res.json())
     .then(data => {
-      getHTMLelements()
+      getHTMLelements();
       data.forEach(getData);
     });
 }
 
-
-const getData = (data) => {
-  (data.projects).forEach(createCardsInCarousel)
-}
+const getData = data => {
+  data.projects.forEach(createCardsInCarousel);
+};
 
 function createCardsInCarousel(e) {
   console.log(e.featuredImage);
   // let a = (document.querySelector(".studentCardTemplate").content).cloneNode(true)
-  const cln = (HTML.studentCardTemplate).cloneNode(true);
+  const cln = HTML.studentCardTemplate.cloneNode(true);
   console.log(cln.querySelector(".imageIntheCarousel"));
   // HTML.textCaption = document.querySelector(".textCaption");
   cln.querySelector(".imageIntheCarousel").src = `./${e.featuredImage}`;
@@ -36,9 +37,8 @@ function createCardsInCarousel(e) {
   //   ".imageAndTextCaption"
   // );
 
-  (HTML.projectImagesAndCaptions).appendChild(cln);
+  HTML.projectImagesAndCaptions.appendChild(cln);
 }
-
 
 // const imageAndTextCaptionConvertedArray = Array.from(imageAndTextCaption);
 // const projectImagesAndCaptions = document.querySelector(".projectImages");
